ca9306d67bbe5ca6cb625b2efdcca61665e5f514
Rafael Gomez <rgomez2014@gmail.com>
2023-09-16 13:35:37 -0500

    router.ger created for all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,34 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+      attributes: {
+        include: [
+          sequelize.literal(
+            '(SELECT COUNT (*) FROM product WHERE product.category_id = categor.id'
+          )
+        ]
+      }
+    });
+    const categories = categoryData.map(category.get({ plain: true }));
+    res.status.json(categories)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // be sure to include its associated Products\
 });
 
 router.post('/', (req, res) => {

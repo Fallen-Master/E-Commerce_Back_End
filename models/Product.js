a30471603bdbfaeb378c1c465c0229886e566ddf
Rafael Gomez <rgomez2014@gmail.com>
2023-09-17 13:08:26 -0500

    Minor updates

--- models/Product.js
+++ models/Product.js
@@ -20,10 +20,11 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
-        isDecimal: true
+        isDecimal: true,
+        min: 0
       },
       stock: {
         type: DataTypes.INTEGER,
